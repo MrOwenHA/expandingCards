8676dfbd9a09fedf1dd7e2b899ded15a0e52b71d
lbowen67 <lbowen67@gmail.com>
2024-01-14 22:48:00 -0500

    testing

--- script.js
+++ script.js
@@ -2,19 +2,22 @@
 // Expanding Cards
 
 
-const panels = document.querySelectorAll('.panel')
+const panels = document.querySelectorAll('.panel');
+
 
 function removeActiveClassess() {
     panels.forEach((panel) => {
-        panel.classList.remove('active')
-    })
+        panel.classList.remove('active');
+    });
 }
 
 panels.forEach((panel) => {
+    console.log(panel);
     panel.addEventListener('click', () => {
-        console.log(panel)
-        removeActiveClassess()
-        panel.classList.add('active')
-    })
-})
+        console.log(panel);
+        removeActiveClassess();
+        panel.classList.add('active');
+        console.log(panels);
+    });
+});
 
